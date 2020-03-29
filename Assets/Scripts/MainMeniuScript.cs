bb05cc4576a2c85a532da13cfaeb499719f559ab
Aurimas <aurimas.kavaliauskas@ktu.edu>
2020-03-29 19:46:30 +0300

    MainMeniu
    MainMeniu basic  UI and Minimal logic

---
+++ Assets/Scripts/MainMeniuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMeniuScript : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void PlayGame()
+    {
+        Debug.Log("Start");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
+public class Globals {
+public static string UserName;
+    public static string UserID;
+    public static string ConectionString;
+}
